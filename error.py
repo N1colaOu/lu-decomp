a5aab2bb073a720f5a3151c9f44e7c397af0bdb6
N1colaOu <n.ouzounov06@gmail.com>
2026-06-18 18:21:40 +0200

    fixed some mistakes and updated plotting

--- error.py
+++ error.py
@@ -2,13 +2,13 @@ import numpy as np
 import lu_decomp as lu
 
 
-def get_error(A, x_exact):
+def get_error(A, x_exact, pivot):
     b = np.matmul(A, x_exact) #we get b
     n = len(A)
 
-    (A, P) = lu.lu_decomp(A, pivot=False)
+    (A, P) = lu.lu_decomp(A, pivot=pivot)
     x = lu.forwards_sub(A, P, b)
-    lu.backwards_sub(A, x) # we get x, which shoudl be the same as x_exact
+    lu.backwards_sub(A, x)
     err = get_norm_2(x-x_exact)/get_norm_2(x_exact)
     return err
 
--- testing_f.py
+++ testing_f.py
@@ -4,38 +4,49 @@ import error as err
 import matplotlib.pyplot as plt
 
 n_arr = linspace(2, 20, 19, dtype=int)
-err_1 = []
-err_hilbert = []
+err_1_with = []
+err_1_without = []
+err_hilbert_with = []
+err_hilbert_without = []
 for n in n_arr:
     x_noise = [1]*n
     A1 = zeros([n, n])
-    for i in range(n):
-        for j in range(n):
-            A1[i, n-j-1] = power(1/3, abs(i-j)) + power(1/2, j+i) + 10e-10
+    for i in range(1, n+1):
+        for j in range(1, n+1):
+            A1[i-1, n-j] = power(1/3, abs(i-j)) + power(1/2, j+i) + 10e-10
 
     Ah = zeros([n, n])
-    for i in range(n):
-        for j in range(n):
-            Ah[i, j] = 1/(i+j+1)
+    for i in range(1, n+1):
+        for j in range(1, n+1):
+            Ah[i-1, j-1] = 1/(i+j+1)
     
-    err_1.append(err.get_error(A1, x_noise))
-    err_hilbert.append(err.get_error(Ah, x_noise))
+    err_1_with.append(err.get_error(A1, x_noise, True))
+    err_1_without.append(err.get_error(A1, x_noise, False))
+    err_hilbert_with.append(err.get_error(Ah, x_noise, True))
+    err_hilbert_without.append(err.get_error(Ah, x_noise, False))
+
 
 plt.figure(1)
-plt.semilogy(n_arr, err_1, label="Matrix 1")
+plt.semilogy(n_arr, err_1_with, label="Matrix 1 With")
+plt.semilogy(n_arr, err_1_without, label="Matrix 1 Without")
 plt.title("Error of Matrix 1: Log plot")
 plt.grid()
+plt.legend()
 plt.savefig('./build/error_1.png')
 
 plt.figure(2)
-plt.semilogy(n_arr, err_hilbert, label="Hilbert Matrix")
+plt.semilogy(n_arr, err_hilbert_with, label="Hilbert Matrix With")
+plt.semilogy(n_arr, err_hilbert_without, label="Hilbert Matrix Without")
 plt.title("Error of Hilbert Matrix: Log plot")
 plt.grid()
+plt.legend()
 plt.savefig('./build/error_hilbert.png')
 
 plt.figure(3)
-plt.semilogy(n_arr, err_1, label="Matrix 1")
-plt.semilogy(n_arr, err_hilbert, label="Hilbert Matrix")
+plt.semilogy(n_arr, err_1_with, label="Matrix 1 With")
+plt.semilogy(n_arr, err_1_without, label="Matrix 1 Without")
+plt.semilogy(n_arr, err_hilbert_with, label="Hilbert Matrix With")
+plt.semilogy(n_arr, err_hilbert_without, label="Hilbert Matrix Without")
 plt.title("Comparison of the Errors: Log Plot")
 plt.legend()
 plt.grid()
